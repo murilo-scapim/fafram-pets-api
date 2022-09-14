7f712fa1e915f0ef3f373b0c9cb1905eb7495be1
Murilo Scapim <mhscapim@gmail.com>
2022-09-14 12:15:14 -0300

    Add pets.sql

---
+++ pets.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS pets_api;
+
+USE pets_api;
+
+CREATE TABLE IF NOT EXISTS pets (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL,
+  species VARCHAR(30) NOT NULL,
+  sex CHAR(1) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO pets (name, species, sex)
+VALUES ('Lola', 'Dog', 'f'),
+('Bela', 'Hamster', 'f'),
+('Kitty', 'Cat', 'f'),
+('Puffball', 'Hamster', 'm');
